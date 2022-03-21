ffb63f9a97cd31fe9cbda535bf5f3c8cc6fdd7fe
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-03-21 18:09:32 +0100

    Inizio sviluppo top banner

--- src/components/TopBanner.vue
+++ src/components/TopBanner.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Test</h1>
+  <div class="top-banner">
+    <div class="container">
+      <h3>Test colori</h3>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,4 +14,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "../assets/scss/colors.scss";
+
+.top-banner {
+  background: $top-banner;
+
+  .container {
+    width: 1000px;
+    min-height: 40px;
+    margin: 0 auto;
+    color: $default;
+    border: 2px dashed red;
+  }
+}
 </style>
